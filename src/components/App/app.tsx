c1afb7a4cc999646330276339cb85b0345b5b625
nima.latifi@gmail.com <nima.latifi@gamil.com>
2020-07-16 17:56:24 +0430

    Add Footer component
    Signed-off-by: nima.latifi@gmail.com <nima.latifi@gamil.com>

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import './app.scss';
 import Header from"../Header/header"
+import Footer from"../Footer/footer"
 export interface IAppProps {}
 
 export default function IApp(props: IAppProps) {
   return (
     <div className="main">
       <Header />
+      <Footer />
     </div>
   );
 } 
